3c6ebbe0d3f3ceb7f67a58637ab49456114b9d80
mador-till-prod <lemidametukshevet@gmail.com>
2022-08-18 09:54:50 +0300

    odt nitzan

--- odt.js
+++ odt.js
@@ -1,64 +1,9 @@
-var Arr_Location = [
-    // the locations in the array are by order
-    {
-        name: `מיקום 1`, // name of the location
-        text: `הסבר על מיקום 1`, 
-        extra_text: `עוד הרבה הסבר`,
-        video: `קישור`,
-        safety: `pdf` 
-    },
-]
-
-$(function() {
-    for (let i = 1; i <= Arr_Location.length; i++) {
-        $("body").prepend(
-            `<div id="location-${i}" class="page">
-                <div class="container">
-                    <div class="title">${Arr_Location[i-1].name}</div>
-                    <div class="text">${Arr_Location[i-1].text}</div>
-                    <center>
-                        <video width="320" height="240" controls>
-                            <source src="movie.mp4" type="${Arr_Location[i-1].video}/mp4">
-                            <source src="movie.ogg" type="${Arr_Location[i-1].video}/ogg">
-                        </video>
-                    </center>
-                </div>
-            </div>`);
-        // locations in the menu
-        $(`#menu`).append(`<div class="menu-item location-${i} button">${Arr_Location[i-1].name}</div>`);
-        // locations on the map
-        $(`#map`).append(`<div class="location location-${i} button"></div>`);
-        // למחוק! זה בשביל שהנקודות על המפה יהיו מפוזרות בנקודות שונות, לא רלוונטי כאשר יש גרפיקות
-        $(`.location-${i}`).css({"top": `${2*i}vh`, "left": `${Math.random() * 60 + 20}vw`});
-    }
-    $(`#menu-button`).on("click", function() {
-        $(`#menu`).css("display", "flex");
-    });
-    [$(`.menu-item`), $(`#x`)].forEach(item => {
-        item.on("click", function() {
-            $(`#menu`).css("display", "none");
-        });
-    });
-    [$(`#map-button`), $(`#home-button`)].forEach(item => {
-        item.on("click", function() {
-            $(".page").css("display", "none");
-            $(`#${item.attr("id").slice(0, -7)}-page`).css("display", "block");
-            $(`#buttons .button`).css("display", "block");
-            item.css("display", "none");
-        });
-    });
-    // $(`#map-button`).on("click", function() {
-    //     $(".page").css("display", "none");
-    //     $(`#map-page`).css("display", "block");
-    // });
-    // show location page
-    [$(".location"), $(".menu-item")].forEach(item => {
-        item.on("click", function() {
-            $(".page").css("display", "none");
-            $(`#location-${item.attr("class").split(/\s+/)[1].slice(-1)}.page`).css("display", "block");
-            $(`#buttons .button`).css("display", "block");
-        });
-    });
+/* loading function
+--------------------------------------------------------------
+Description: */
+window.addEventListener("load", () => { 
+    document.querySelector(".mapButton").addEventListener("click", () => {
+        document.querySelector("#home-page").classList.add("hidden")
+        document.querySelector("#map-page").classList.remove("hidden")
+    })
 });
-
-//element.attr('class').match(/my_widget-(\d+)/)[1]
