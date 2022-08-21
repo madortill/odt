fec8ec7122390c7c3447bb1e0a802913b7d88749
mador-till-prod <lemidametukshevet@gmail.com>
2022-08-21 16:00:01 +0300

    check switch html

--- odt.js
+++ odt.js
@@ -2,8 +2,22 @@
 --------------------------------------------------------------
 Description: */
 window.addEventListener("load", () => { 
-    document.querySelector(".mapButton").addEventListener("click", () => {
-        document.querySelector("#home-page").classList.add("hidden")
-        document.querySelector("#map-page").classList.remove("hidden")
+    if(window.location.pathname.includes("odt")){
+        document.querySelector(".mapButton").addEventListener("click", () => {
+            document.querySelector("#home-page").classList.add("hidden")
+            document.querySelector("#map-page").classList.remove("hidden")
+        })
+    }
+
+    document.querySelectorAll(".station").forEach(station => {
+        station.addEventListener("click", switchHtml);
     })
 });
+
+/* switchHtml
+--------------------------------------------------------------
+Description: */
+const switchHtml = (event) => {
+    console.log(event.currentTarget.classList[1])
+    window.location.pathname = `/${event.currentTarget.classList[1]}.html`
+};
