291151db7e757301d8596c1f15dc06524057334d
mador-till-prod <lemidametukshevet@gmail.com>
2022-08-22 10:05:32 +0300

    finish!

--- odt.js
+++ odt.js
@@ -2,22 +2,32 @@
 --------------------------------------------------------------
 Description: */
 window.addEventListener("load", () => { 
-    if(window.location.pathname === "/odt/odt.html"){
-        document.querySelector(".mapButton").addEventListener("click", () => {
-            document.querySelector("#home-page").classList.add("hidden")
-            document.querySelector("#map-page").classList.remove("hidden")
-        })
-    }
-
+    document.querySelectorAll(".mapButton").forEach(mapButton => {
+        mapButton.addEventListener("click", showMap);
+    })
     document.querySelectorAll(".station").forEach(station => {
-        station.addEventListener("click", switchHtml);
+        station.addEventListener("click", (event) => {
+            window.location.pathname = `/odt/${event.currentTarget.classList[1]}.html`
+        });
+    })
+    document.querySelector(".menuButton").addEventListener("click", () => {
+        document.querySelector(".menuPage").classList.remove("hidden");
+        document.querySelector(".exitMenuButton").addEventListener("click", () => {
+            document.querySelector(".menuPage").classList.add("hidden")
+        })
     })
 });
 
-/* switchHtml
+/* showMap
 --------------------------------------------------------------
 Description: */
-const switchHtml = (event) => {
-    console.log(event.currentTarget.classList[1])
-    window.location.pathname = `/odt/${event.currentTarget.classList[1]}.html`
+const showMap = (event) => {
+    let page = event.currentTarget.classList[1]
+    document.querySelector(`#${page}`).classList.add("hidden")
+    document.querySelector(".menuPage").classList.add("hidden")
+    document.querySelector("#map-page").classList.remove("hidden")
+    document.querySelector(".exitButton").addEventListener("click", () => {
+        document.querySelector(`#${page}`).classList.remove("hidden")
+        document.querySelector("#map-page").classList.add("hidden")
+    })
 };
